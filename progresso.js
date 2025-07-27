18707975058db24b24e9a79b4c30b851921497ff
Marsseloh <marcelosantanacorreia2008@gmail.com>
2025-07-27 00:40:46 -0300

    Barra de progresso(funcionalidades)

---
+++ progresso.js
@@ -0,0 +1,29 @@
+firebase.initializeApp(firebaseConfig);
+async function contadorProgresso(checkbox, porcentagemVideo, nomeModulo) {
+    if (checkbox.checked) {
+        firebase.auth().onAuthStateChanged(async function (user) {
+            if (user) {
+                const uid = user.uid;
+                const docRef = db.collection("usuarios").doc(uid).collection("progresso").doc(nomeModulo);
+                try {
+                    const docSnap = await docRef.get();
+                    if (docSnap.exists) {
+                        if (docSnap.data().porcentagem < 100) { var porcentagemAtual = docSnap.data().porcentagem + porcentagemVideo; }
+                        db.collection("usuarios").doc(uid).collection("progresso").doc(nomeModulo).set({
+                            porcentagem: porcentagemAtual
+                        }, { merge: true });
+                    } else {
+                        porcentagemAtual = porcentagemVideo
+                        db.collection("usuarios").doc(uid).collection("progresso").doc(nomeModulo).set({
+                            porcentagem: porcentagemAtual
+                        }, { merge: true });
+                    }
+                } catch (error) {
+                    console.error("Erro ao buscar documento:", error);
+                }
+            } else {
+                console.log("Nenhum usuário logado");
+            }
+        });
+    }
+}
