ea3af3688d10f6bca5713e200a7f684d2bf5b613
Marsseloh <marcelosantanacorreia2008@gmail.com>
2025-09-11 13:33:25 -0300

    correção de bug de de redirecionamento para login v3

--- src/js/DAL.js
+++ src/js/DAL.js
@@ -33,7 +33,7 @@ firebase.auth().onAuthStateChanged((user) => {
         loginButtonOffline.style.display = 'none';
         loginButtonOnline.style.display = 'block';
         // Ex: mostrar conteúdo exclusivo ou redirecionar
-    } else if (!caminho.includes('paginaLogin')) {
+    } else if (!caminho.includes('paginalogin')) {
         // Usuário não está logado
         // Ex: redirecionar para tela de login
         if (caminho.includes('index') || caminho.includes('modulos')) {
